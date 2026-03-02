63bbb85d804beb0541b266a7d18458605b88616d
BashCommandManager User <user@bashcommandmanage.com>
2026-03-02 21:20:18 +0800

    feat: create main window with HandyControl layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,27 @@
+using BashCommandManager.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
 namespace BashCommandManager;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
+        DataContext = viewModel;
+
+        Loaded += async (s, e) =>
+        {
+            await viewModel.GroupTreeViewModel.LoadGroupsAsync();
+        };
+    }
+
+    private void GroupTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        if (DataContext is MainViewModel vm && e.NewValue is Core.Models.Group group)
+        {
+            vm.GroupTreeViewModel.SelectedGroup = group;
+        }
     }
 }

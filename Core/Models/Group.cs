3e4d034756822647d8f403194a4f5f7c03fef38a
BashCommandManager User <user@bashcommandmanage.com>
2026-03-02 22:22:57 +0800

    feat: Group 模型添加 IsEditing 属性支持就地编辑

--- Core/Models/Group.cs
+++ Core/Models/Group.cs
@@ -1,11 +1,16 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace BashCommandManager.Core.Models;
 
-public class Group
+public partial class Group : ObservableObject
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int? ParentId { get; set; }
     public int SortOrder { get; set; }
+
+    [ObservableProperty]
+    private bool _isEditing;
 
     // 导航属性
     public List<Group> Children { get; set; } = new();

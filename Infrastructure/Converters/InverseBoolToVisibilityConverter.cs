2adb22641652af77c16aab8b84c356868ab843d5
BashCommandManager User <user@bashcommandmanage.com>
2026-03-08 00:06:24 +0800

    fix: 修复设置窗口闪退问题
    修复 SettingsWindow.xaml 中错误的 XAML 资源键名引用：
    - 将 BoolToVisibilityConverter 改为 BoolToVisibility
    - 添加缺失的 BooleanToVisibilityConverter 和 InverseBoolToVisibilityConverter 资源定义

---
+++ Infrastructure/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BashCommandManager.Infrastructure.Converters;
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public class InverseBoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+        return Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibility)
+        {
+            return visibility != Visibility.Visible;
+        }
+        return false;
+    }
+}
